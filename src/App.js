4a02c13e34a3738669cddf82f415acbb16be4b9a
efucaraccio <edufuca@gmail.com>
2022-03-14 20:42:48 -0300

    Mint operation

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 import RLogin, { RLoginButton } from '@rsksmart/rlogin'
 import WalletConnectProvider from '@walletconnect/web3-provider'
 import { useState } from 'react';
-
+import Web3 from 'web3';
+import MocAbi from './Contract.json';
+import MocConnectorAbi from './MocConnector.json';
  // construct rLogin pop-up in DOM
 const rLogin = new RLogin({
   cachedProvider: false, // change to true to cache user's wallet choice
@@ -25,19 +27,53 @@ function App() {
   
   const [provider, setProvider] = useState(null)
   const [account, setAccount] = useState(null)
+  const [isLogin, setisLogin] = useState(false)
 
     // display pop up
   const connect = () => rLogin.connect()
     .then(({ provider }) => { // the provider is used to operate with user's wallet
       setProvider(provider)
       // request user's account
-      provider.request({ method: 'eth_accounts' }).then(([account]) => setAccount(account))
+      provider.request({ method: 'eth_accounts' }).then(([account]) => 
+      {setAccount(account); 
+       setisLogin(true);
+      })
     })
+    const callback = (error, transactionHash) => {
+      console.log(transactionHash);
+      console.log('Mint done ' + transactionHash);
+    }
 
+    const HardcodedMint = async (e) => {
+      e.preventDefault();
+      const web3 = new Web3(provider);
+      const getContract = (abi, contractAddress) => new web3.eth.Contract(abi, contractAddress);
+      const moc = getContract(MocAbi.abi, '0x01AD6f8E884ed4DDC089fA3efC075E9ba45C9039');
+      const connectorAddress = await moc.methods.connector().call();
+      const mocConnector = getContract(MocConnectorAbi.abi, connectorAddress);
+
+      return moc.methods.mintBProVendors(1000000000000000, '0xdda74880d638451e6d2c8d3fc19987526a7af730').send(
+        {
+          from: '0x371E637DE56De8971E6C75a17977D48862eae53E',
+          value: 1001500000000000,
+          gasPrice: 72983680,
+          gas: 1036684,
+          gasLimit: 1036684
+        },
+        callback
+      );
+    }
+   
   return (
-    <div className="App">
-      <RLoginButton onClick={connect}>Connect wallet</RLoginButton>
-      <p>wallet address: {account}</p>
+    <div>
+      <div className="App">
+        <RLoginButton onClick={connect}>{isLogin ? "Connected wallet" : "Connect wallet"}</RLoginButton>
+        <p>wallet address: {account}</p>
+      </div>
+      {isLogin &&
+      <form onSubmit={HardcodedMint}>
+      <button>Hardcoded Mint</button>
+      </form>}
     </div>
   );
 }
